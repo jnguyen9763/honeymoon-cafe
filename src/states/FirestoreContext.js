d3fe1e1e9d2765fd42f6b3921b9a5fb5609ca78b
Thu Cao <caonthu@gmail.com>
2022-08-23 02:19:19 -0400

    added orderNumber to collection

--- src/states/FirestoreContext.js
+++ src/states/FirestoreContext.js
@@ -1,18 +1,29 @@
 import { STATUSES } from "../constants/statuses";
 import React, { createContext, useState } from "react";
 import { db } from "../firebase";
-import { addDoc, collection } from "@firebase/firestore"
+import { doc, setDoc, getDoc, updateDoc } from "@firebase/firestore"; 
 
 const FirestoreContext = createContext({});
 
 export const FirestoreProvider = ({ children }) => {
   const [orders, setOrders] = useState([]);
   const [orderNumber, setOrderNumber] = useState(1);
-  const ref = collection(db, "orders");
+  const orderRef = doc(db, "orders", orderNumber.toString());
+  const orderNumRef = doc(db, "orderNumber", "orderNumber");
+
+  // TODO: figure out when we want to initialize this DB
+  // Maybe have it in the dashboard to reset everything?
+  //  i.e delete all documents in orders, set order number to 0
+  const initializeDb = () => {
+    setDoc(orderNumRef, { orderNumber: 0 });
+  };
 
   const createOrder = async ({ items, notes, paymentMethod, totalAmount }) => {
+    const orderNumSnap = await getDoc(orderNumRef);
+    let orderNum = orderNumSnap.get("orderNumber") + 1;
+  
     let newOrder = {
-      id: orderNumber, 
+      id: orderNum, 
       items: Object.keys(items).map((name) => ({
           name,
           quantity: items[name].quantity,
@@ -25,14 +36,19 @@ export const FirestoreProvider = ({ children }) => {
     };
 
     try {
-      await addDoc(ref, newOrder);
+      await setDoc(orderRef, newOrder);
     } catch (e) {
-      console.log(e);
+      console.log("Error creating new order: ", e);
+    }
+
+    try {
+      await setDoc(orderNumRef, {orderNumber : orderNum});
+    } catch (e) {
+      console.log("Error updating order number: ", e);
     }
 
     setOrders([...orders, newOrder]);
-    setOrderNumber(orderNumber + 1);
-    
+    setOrderNumber(orderNum);
     
     // The original JS way
     /*
@@ -55,7 +71,16 @@ export const FirestoreProvider = ({ children }) => {
     */
   };
 
-  const updateOrderStatus = (orderNumber, newStatus) => {
+  // UPDATE orders SET status == newStatus where orderNumber == orderNum
+  // TODO: the updates button currently not working - orderNumber might not be in sync
+  // might need to figure out how to initialize the db first or just do it manually
+  // in the firebase browser
+  // - figure out a more efficient way to update the "global" order array without having
+  // to loop through the whole list of orders
+  const updateOrderStatus = async (orderNumber, newStatus) => {
+    const orderRef = doc(db, "orders", orderNumber);
+    await updateDoc(orderRef, { status: newStatus });
+
     const newOrders = orders.map((order) => {
       if (order.orderNumber === orderNumber) {
         return { ...order, status: newStatus };
