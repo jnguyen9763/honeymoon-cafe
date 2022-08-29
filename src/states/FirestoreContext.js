6e3f3cca6e0dbfb13b06887d292f1990101fb2df
Jacqueline Nguyen <jnguyen9763@gmail.com>
2022-08-29 17:22:33 -0400

    merge BE code

--- src/states/FirestoreContext.js
+++ src/states/FirestoreContext.js
@@ -1,78 +1,83 @@
 import { STATUSES } from "../constants/statuses";
-import React, { createContext } from "react";
-import { doc, collection } from 'firebase/firestore';
-import { useDocument, useCollection } from 'react-firebase-hooks/firestore'
+import {
+  collection,
+  doc,
+  increment,
+  deleteDoc,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
 import { firestore } from "../firebase";
-import { useAlert } from "../hooks/useAlert";
-import { Spinner } from 'reactstrap'
-import LoadingScreen from '../views/LoadingScreen'
+import { useDocument, useCollection } from "react-firebase-hooks/firestore";
+import ErrorScreen from "../views/ErrorScreen";
+import LoadingScreen from "../views/LoadingScreen";
+import React, { createContext } from "react";
 
 const FirestoreContext = createContext({});
 
+const orderNumRef = doc(firestore, "orderNumber", "orderNumber");
+
 export const FirestoreProvider = ({ children }) => {
-  const [orderNumVal, orderNumLoading, orderNumError] = useDocument(
-    doc(firestore, 'orderNumber', 'orderNumber'),
+  const [orderNumValue, orderNumLoading, orderNumError] = useDocument(
+    doc(firestore, "orderNumber", "orderNumber")
+  );
+  const [ordersValue, ordersLoading, ordersError] = useCollection(
+    collection(firestore, "orders")
   );
 
-  const [ordersVal, ordersLoading, ordersError] = useCollection(
-    collection(firestore, 'orders'),
-  );
-    
-  if (orderNumError || ordersError ) {
-    return <div>
-          {ordersError && <strong>Error: {JSON.stringify(ordersError)}</strong>}
-          {orderNumError && <strong>Error: {JSON.stringify(orderNumError)}</strong>}
-    </div>
-  }  
-
-
-  if (ordersLoading || orderNumLoading) {
-    return <LoadingScreen/>
+  if (orderNumError || ordersError) {
+    return (
+      <ErrorScreen orderNumError={orderNumError} ordersError={ordersError} />
+    );
   }
 
-  const { orderNumber } = orderNumVal.data()
+  if (orderNumLoading || ordersLoading) {
+    return <LoadingScreen />;
+  }
 
-  const orders = ordersVal.docs.map((doc) => doc.data())
+  const { orderNumber } = orderNumValue.data();
 
-  console.log(orders)
+  const orders = ordersValue.docs.map((doc) => doc.data());
 
-  const createOrder = ({ items, notes, paymentMethod, totalAmount }) => {
-    // setOrders([
-    //   ...orders,
-    //   {
-    //     barista: undefined,
-    //     items: Object.keys(items)
-    //       .map((name) => ({
-    //         name,
-    //         quantity: items[name].quantity,
-    //       }))
-    //       .sort((a, b) => a.name.localeCompare(b.name)),
-    //     notes,
-    //     orderNumber,
-    //     paymentMethod,
-    //     status: STATUSES.NEW,
-    //     totalAmount,
-    //   },
-    // ]);
-    // setOrderNumber(orderNumber + 1);
+  const createOrder = async ({ items, notes, paymentMethod, totalAmount }) => {
+    const orderRef = doc(firestore, "orders", `${orderNumber}`);
+
+    const newOrder = {
+      barista: null,
+      items: Object.keys(items)
+        .map((name) => ({
+          name,
+          quantity: items[name].quantity,
+        }))
+        .sort((a, b) => a.name.localeCompare(b.name)),
+      notes,
+      orderNumber,
+      paymentMethod,
+      status: STATUSES.NEW,
+      totalAmount,
+    };
+
+    await setDoc(orderRef, newOrder);
+    await updateDoc(orderNumRef, { orderNumber: increment(1) });
   };
 
-  const updateOrderProperties = (orderNumber, newProperties) => {
-    const newOrders = orders.map((order) => {
-      if (order.orderNumber === orderNumber) {
-        return { ...order, ...newProperties };
-      }
+  const resetDatabase = async () => {
+    await updateDoc(orderNumRef, { orderNumber: 1 });
+    for (const doc of ordersValue.docs) {
+      await deleteDoc(doc.ref);
+    }
+  };
 
-      return order;
-    });
-
-    // setOrders(newOrders);
+  const updateOrderProperties = async (orderId, newProperties) => {
+    const orderRef = doc(firestore, "orders", `${orderId}`);
+    await updateDoc(orderRef, newProperties);
   };
 
   const contextValue = {
     orders,
     orderNumber,
     createOrder,
+    resetDatabase,
     updateOrderProperties,
   };
 
